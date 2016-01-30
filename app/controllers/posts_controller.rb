1e9f550de12389254e6bb7d1c1950f1780b24d4a
Joseph Mokenela <joseph.mokenela@gmail.com>
2016-01-30 05:58:41 +0200

    added some forms for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,23 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to @post
+	end
+
+	def show
 		
+	end
+
+	private 
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
